9e3e5a50fd19cb1664f87bd86eab6d506796c708
Davi <davi.frrs@gmail.com>
2023-10-10 11:44:04 -0300

    feat: Added ApiKey Auth

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,8 @@ namespace Blog;
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "ZTgzMTJiYTEtZjdmMy00NmY4LWI0MmUtNDNiMGMyYjI1MGI5";
+    public static readonly string JwtKey = "ZTgzMTJiYTEtZjdmMy00NmY4LWI0MmUtNDNiMGMyYjI1MGI5";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_etaioejgioarETYIAE//==";
 
 }
